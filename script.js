9c09834328c77efdd5ad4c328c3ab14aa49734c9
Mian Mohid Naeem <113250876+mrcoder-portfolio@users.noreply.github.com>
2023-04-07 16:42:37 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const words = 'The quick brown box jump over the lazy dog'.split(' ')
+const words = [
+    'Once upon a time, there was a little girl named Alice who loved to read books. She spent most of her time buried in novels, dreaming of the adventures she could have if only they were real. One day, as she was walking home from the library, she stumbled upon a mysterious key lying on the sidewalk.',
+    "After years of searching, the treasure hunter finally discovered the long-lost artifact. But as he reached out to take it, he realized too late that it was booby-trapped.",
+    "The young musician had always dreamed of playing at Carnegie Hall. Finally, her big break came - but just as she was about to step onto the stage, she realized she had forgotten her sheet music.",
+    "Every day, the old man sat on the same bench in the park, feeding the pigeons. But one day, a small bird landed on his shoulder and changed his life forever.",
+    "As the storm raged outside, the group of strangers huddled together in the abandoned cabin, wondering how they would survive the night. But when the power went out and the phone lines went dead, they knew they were in real trouble.",
+    "The detective had been working on the case for months, following lead after lead. But it wasn't until he received a cryptic message from an unknown source that he realized he was closer to the truth than he ever imagined.",
+]
 const wordsCount = words.length
-const gameTime = 30
+let gameTime = document.getElementById('SelectTime').value
+document.getElementById('SelectTime').addEventListener('change', (e) => {
+    gameTime = e.target.value
+    document.getElementById('info').innerHTML = gameTime
+})
+
 window.timer = null
 window.gameStart = null
 function addClass(el, name) {
@@ -12,19 +24,20 @@ function removeClass(el, name) {
 
 function randomWord() {
     const randomIndex = Math.ceil(Math.random() * words.length - 1)
-    return words[randomIndex]
+    const para = words[randomIndex].split(' ')
+    return para
 }
 
 function formatWord(word) {
     return `<div class="word"><span class="letter">${word.split("").join('</span><span class="letter">')}</span></div>`
 }
 
-function getWPM(){
+function getWPM() {
     const words = [...document.querySelectorAll('.word')]
     const lastTypedWord = document.querySelector('.word.current')
     const lastTypedWordIndex = words.indexOf(lastTypedWord)
-    const typedWords = words.slice(0 , lastTypedWordIndex)
-    const correctWords = typedWords.filter(word =>{
+    const typedWords = words.slice(0, lastTypedWordIndex)
+    const correctWords = typedWords.filter(word => {
         const letters = [...word.children]
         const incorrectLetters = letters.filter(letter => letter.className.includes('incorrect'))
         const correctLetters = letters.filter(letter => letter.className.includes('correct'))
@@ -35,15 +48,18 @@ function getWPM(){
 
 function gameOver() {
     clearInterval(window.timer)
-    addClass(document.getElementById('game') , 'over')
+    addClass(document.getElementById('game'), 'over')
     document.getElementById('info').innerHTML = `WPM: ${getWPM()}`
 }
 
 
 function newGame() {
     document.getElementById('words').innerHTML = ''
-    for (let i = 0; i < 200; i++) {
-        document.getElementById('words').innerHTML += formatWord(randomWord())
+    for (let i = 0; i < 10; i++) {
+        const para = randomWord()
+        for (let j = 0; j < para.length; j++) {
+            document.getElementById('words').innerHTML += formatWord(para[j])
+        }
     }
     addClass(document.querySelector('.word'), 'current');
     addClass(document.querySelector('.letter'), 'current');
@@ -54,6 +70,7 @@ function newGame() {
     cursor.style.left = (nextLetter || nextWord).getBoundingClientRect().left + 2 + 'px'
     document.getElementById('info').innerHTML = gameTime
     window.timer = null;
+    window.gameStart = null;
 }
 
 document.getElementById('game').addEventListener('keyup', (e) => {
@@ -66,7 +83,7 @@ document.getElementById('game').addEventListener('keyup', (e) => {
     const isBackSpace = key === 'Backspace'
     const isFirstLetter = currentLetter === currentWord.firstChild;
 
-    if (document.querySelector('#game.over')){
+    if (document.querySelector('#game.over')) {
         return
     }
 
@@ -83,7 +100,7 @@ document.getElementById('game').addEventListener('keyup', (e) => {
                 return
             }
             document.getElementById('info').innerHTML = sleft + ''
-            
+
         }, 1000)
 
     }
@@ -151,9 +168,11 @@ document.getElementById('game').addEventListener('keyup', (e) => {
     cursor.style.left = (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? 'left' : 'right'] + 2 + 'px'
 })
 
-document.getElementById('newGameButton').addEventListener('click' , ()=>{
+document.getElementById('newGameButton').addEventListener('click', () => {
     gameOver()
     document.getElementById('game').className = ''
+    const words = document.getElementById('words')
+    words.style.marginTop = '0px'
     newGame()
 })
 newGame()
